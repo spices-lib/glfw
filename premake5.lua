90d9cafb6c9653a3978999ee36388bbed3367003
Spices <spiecs@outlook.com>
2026-03-26 22:54:04 +0800

    Update premake5.lua

--- premake5.lua
+++ premake5.lua
@@ -106,7 +106,8 @@ project "GLFW"
 		runtime "Debug"
 		symbols "on"
 		sanitize { "Address" }
-		flags { "NoRuntimeChecks", "NoIncrementalLink" }
+		runtimechecks "Off"
+		incrementallink "Off"
 
 	filter "configurations:Release"
 		runtime "Release"
